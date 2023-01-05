7b854b3203783fc52d73f5f736c435c64c94298b
Ylljete Kicaj <118022552+ylljetakicaj@users.noreply.github.com>
2023-01-05 01:33:59 +0100

    Update registerfile.v

--- code./registerfile.v
+++ code./registerfile.v
@@ -1,1 +1,33 @@
+module RegisterFile(
+input wire[3:0] RS,
+input wire[3:0] RT,
+input wire[3:0] RD,
+input wire[23:0] WriteData,
+input wire RegWrite,
+input wire Clock,
+output wire[23:0] ReadRS,
+output wire[23:0] ReadRT
+    );
+    
+reg[23:0] Registers[23:0];
 
+//Reseto te gjithe regjistrat ne 0
+integer i;
+initial 
+begin
+for(i=0;i<23;i=i+1)
+   Registers[i] <= 23'd0; 
+end
+
+//Shkruaj ne regjiter
+always @(posedge Clock)
+begin
+Registers[RD] <= WriteData;
+end
+
+//lexo regjistrat ReadData1, ReadData2
+assign ReadRS = Registers[RS];
+assign ReadRT = Registers[RT];
+
+
+endmodule
