c89189bb927a2f397295b81ac5130d916b130315
SuhejlaHoxha <121345932+SuhejlaHoxha@users.noreply.github.com>
2023-01-05 23:09:43 +0100

    Create CPUtest.v

---
+++ Test/CPUtest.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/05/2023 11:03:03 PM
+// Design Name: 
+// Module Name: CPUtest
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module CPUtest();
+
+reg Clock;
+
+integer i;
+initial
+begin
+for(i=0; i < 30; i=i+1) //30x nderro nga Clock 0-1, 30 tehe pozitive
+begin
+#10 Clock = 0;
+#10 Clock = 1;
+end
+
+#10 $stop;
+end
+
+CPU CPU24(Clock);
+endmodule
