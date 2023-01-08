f41dfd4a4e43c7a03682f9bc363a7d88a0e78676
Tringa Baftiu <97124034+Triinga@users.noreply.github.com>
2023-01-08 09:47:25 +0100

    Create instruction_memory.v

---
+++ code./instruction_memory.v
@@ -0,0 +1,16 @@
+
+module InstructionMemory(
+input wire[23:0] PCAddress,
+output wire[23:0] Instruction);
+
+reg[7:0] instrMem[127:0];
+
+initial
+$readmemb("instructionMemory.mem", instrMem);
+
+assign Instruction[23:16] = instrMem[PCAddress];
+assign Instruction[15:8] = instrMem[PCAddress + 32'd1];
+ assign Instruction[7:0] = instrMem[PCAddress + 32'd2];
+
+
+endmodule
