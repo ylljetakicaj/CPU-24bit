9506d53ad61fdf037621e407b39e48daeea251cd
Ylljete Kicaj <118022552+ylljetakicaj@users.noreply.github.com>
2023-01-10 00:05:56 +0100

    Create RegisterFile.v

---
+++ code./RegisterFile.v
@@ -0,0 +1,55 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/09/2023 11:39:44 PM
+// Design Name: 
+// Module Name: RegisterFile
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module RegisterFile(
+input wire[3:0] RS,
+input wire[3:0] RT,
+input wire[3:0] RD,
+input wire[23:0] WriteData,
+input wire RegWrite,
+input wire Clock,
+output wire[23:0] ReadRS,
+output wire[23:0] ReadRT
+    );
+    
+reg[23:0] Registers[15:0];
+
+//Reseto te gjithe regjistrat ne 0
+integer i;
+initial 
+begin
+for(i=0;i<16;i=i+1)
+   Registers[i] <= 16'd0; 
+end
+
+//Shkruaj ne regjiter
+always @(posedge Clock)
+begin
+Registers[RD] <= WriteData;
+end
+
+//lexo regjistrat ReadData1, ReadData2
+assign ReadRS = Registers[RS];
+assign ReadRT = Registers[RT];
+
+
+endmodule
