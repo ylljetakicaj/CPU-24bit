3b98bccedb1ec7eb7368a463dcb66f215dd4228f
Tringa Baftiu <97124034+Triinga@users.noreply.github.com>
2023-01-12 15:00:59 +0100

    Update ALU1B.v

--- Code/ALU1B.v
+++ Code/ALU1B.v
@@ -32,7 +32,7 @@ module ALU1(
     output CarryOut
     );
     
-   wire JoB, mB, dhe_teli, ose_teli, mb_teli, XOR_teli, sll_teli, slt_teli, mul_teli; 
+   wire JoB, mB, dhe_teli, ose_teli, mb_teli, XOR_teli,  slt_teli;
    
    assign JoB = ~B;
    
@@ -41,13 +41,12 @@ module ALU1(
  
    assign dhe_teli = A & mB;
    assign ose_teli = A | mB;
-//   assign XOR_teli = A ^ mB;
-//   assign sll_teli = A;
-//   assign slt_teli = A < mB ? 1 : 0;
+   assign XOR_teli = A ^ mB;
+   assign slt_teli = A < mB ? 1 : 0;
    
    mbledhesi m1(A, mB, CIN, mb_teli, CarryOut);
    
-   mux4ne1 MUX(dhe_teli, ose_teli, mb_teli, Less, Op, Result);
+    mux6ne1 MUX(dhe_teli, ose_teli, mb_teli, slt_teli, XOR_teli, Less, Op, Result);
     
     
 endmodule
