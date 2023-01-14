b880ea6b502afe2a66afa2d24611709e38b3a89a
Ylljete Kicaj <118022552+ylljetakicaj@users.noreply.github.com>
2023-01-14 18:23:49 +0100

    Update instruction_memory.v

--- Code/instruction_memory.v
+++ Code/instruction_memory.v
@@ -8,9 +8,9 @@ reg[7:0] instrMem[127:0];
 initial
 $readmemb("instructionMemory.mem", instrMem);
 
-assign Instruction[23:16] = instrMem[PCAddress];
-assign Instruction[15:8] = instrMem[PCAddress + 32'd1];
-assign Instruction[7:0] = instrMem[PCAddress + 32'd2];
+  assign Instruction[23:16] = instrMem[PCAddress + 24'd0];
+  assign Instruction[15:8] = instrMem[PCAddress + 24'd1];
+  assign Instruction[7:0] = instrMem[PCAddress + 24'd2];
 
 
 endmodule
