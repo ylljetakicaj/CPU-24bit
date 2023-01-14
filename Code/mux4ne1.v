3ab03f9b24808243788d9da336f77910ce0fd922
Ylljete Kicaj <118022552+ylljetakicaj@users.noreply.github.com>
2023-01-14 18:25:12 +0100

    Update mux4ne1.v

--- Code/mux4ne1.v
+++ Code/mux4ne1.v
@@ -1,1 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/22/2022 05:30:27 PM
+// Design Name: 
+// Module Name: mux4ne1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mux4ne1(
+    input Hyrja0,
+    input Hyrja1,
+    input Hyrja2,
+    input Hyrja3,
+    input [1:0] S,
+    output Dalja
+    );
+    
+    assign Dalja = S[1] ? (S[0] ? Hyrja3 : Hyrja2) : (S[0] ? Hyrja1 : Hyrja0);
+    
+endmodule
 
